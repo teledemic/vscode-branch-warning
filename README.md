8ee07f2aefdbf5d017885657b4111c3e5de7226f
Carl I <carl@teledemic.com>
2017-11-08 16:50:42 -0500

    Add icon Fix screenshot Better readme Add suppressPopup config

--- README.md
+++ README.md
@@ -2,17 +2,23 @@
 
 Alerts you when you are working on a branch you have designated "protected" such as master. Can help to remember not to commit to the wrong branch.
 
-\!\[screenshot\]\(images/warning.png\)
+![screenshot](images/warning.png)
 
 ## Extension Settings
 
-Include if your extension adds any VS Code settings through the `contributes.configuration` extension point.
-
-For example:
-
 This extension contributes the following settings:
+
+* `branchwarnings.suppressPopup`: Suppress the more intrusive warning dialog when first switching to a protected branch (only show the warning on the status bar).
 
 * `branchwarnings.protectedBranches`: An array of branch names. If it exists, warnings will be shown for these names.
 
-For example if there is an image subfolder under your extension project workspace:
+By default, no warnings will be given. If you wish to warn when working on the branch "master", add to your workspace or user settings:
+```
+	"branchwarnings.protectedBranches": [
+		"master"
+	]
+```
 
+## Known issues
+
+The VSCode git plugin handles workspaces in subfolders of git repositories. This extension does not, and will only activate if the workspace is the root of a git repo.
--- src/extension.ts
+++ src/extension.ts
@@ -1,51 +1,57 @@
 'use strict';
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import * as path from "path";
 import * as fs from "fs";
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+let protectedBranches: string[] = [];
+let suppressPopup = false;
+
 export function activate(context: vscode.ExtensionContext) {
     const status = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 9999);
     status.text = "WARNING";
     status.color = "#f00";
+    status.tooltip = "This branch has been marked as protected in the branch-warnings extension";
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "branch-warnings" is now active!');
+    console.log("Extension branch-warnings initializing");
 
-    let protectedBranches = vscode.workspace.getConfiguration("branchwarnings").get<string[]>("protectedBranches");
-
+    let config = vscode.workspace.getConfiguration("branchwarnings");
+    protectedBranches = config.get<string[]>("protectedBranches");
+    suppressPopup = config.get<boolean>("suppressPopup");
+    
     const gitpath = path.join(vscode.workspace.rootPath, ".git");
     const headpath = path.join(gitpath, "HEAD");
     const pattern = new vscode.RelativePattern(gitpath, "HEAD");
     const watcher = vscode.workspace.createFileSystemWatcher(pattern);
     watcher.onDidChange(e => {
-        updateBranch(status, e.fsPath, protectedBranches);
+        console.log(".git/HEAD change detected");
+        updateBranch(status, e.fsPath);
     });
     vscode.workspace.onDidChangeConfiguration(e => {
+        console.log("Configuration change detected");
         protectedBranches = vscode.workspace.getConfiguration("branchwarnings").get<string[]>("protectedBranches");
-        updateBranch(status, headpath, protectedBranches);
+        updateBranch(status, headpath);
     });
-    updateBranch(status, headpath, protectedBranches);
+    updateBranch(status, headpath);
 }
 
 let lastBranch = "";
 
-function updateBranch(status: vscode.StatusBarItem, path: string, protectedBranches: string[]): void {
+function updateBranch(status: vscode.StatusBarItem, path: string): void {
     fs.readFile(path, "utf-8", (err, data) => {
         if (!err) {
             const line = data.split(/\r\n|\r|\n/)[0];
             const branch = line.split("/").pop();
+            console.log("On branch " + branch);
             if (protectedBranches.some(val => val.toLowerCase() === branch.toLowerCase())) {
+                console.log("Branch is in protected branches [ " + protectedBranches.join(", ") + " ]");
                 status.text = "WARNING: branch is " + branch;
                 status.show();
-                if (lastBranch != branch) {
+                if (lastBranch != branch && !suppressPopup) {
+                    console.log("Branch is a change, showing info");
                     vscode.window.showWarningMessage("WARNING: you are on the protected branch " + branch);
                 }
             } else {
+                console.log("Branch is not in protected branches [ " + protectedBranches.join(", ") + " ]");
                 status.hide();
             }
             lastBranch = branch;
@@ -56,6 +62,5 @@ function updateBranch(status: vscode.StatusBarItem, path: string, protectedBranc
     });
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {
 }
